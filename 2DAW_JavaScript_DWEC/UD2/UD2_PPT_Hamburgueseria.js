be7e9f5b88a6764e36066fa9741b441bf14f1367
Raul Gomez <raaulgs@gmail.com>
2023-10-10 14:16:51 +0200

    DIW_ejerUD1 y plantillas de la memoria en office

---
+++ 2DAW_JavaScript_DWEC/UD2/UD2_PPT_Hamburgueseria.js
@@ -0,0 +1,25 @@
+ /* 0. Pide PEDIDO y dar PRECIO. Programa- 
+ 
+ 1. DEFINIR CLASES: Bebidas/ Hamburguesas/ Patatas/ Nuggets/ Postre (helado, manzana) 
+    Cantidad, precio.
+ 2. METODOS 
+    Metodo ingrediente: Que cosas tiene la hamburguesa. 
+    Calcular precio. */
+
+    
+class Pedido {}
+
+
+
+class Hamburguesa {
+    constructor(nombre){
+        this.nombre = nombre
+    }
+}
+
+class Bebida {
+
+}
+class
+
+// 2. Interacción con el usuario. Como hace el usuario. 
