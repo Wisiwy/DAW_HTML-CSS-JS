bffb14be63278d66f19ba6e760e83e0ceae0aaca
RaulGS <119356579+Wisiwy@users.noreply.github.com>
2023-12-10 22:17:37 +0100

    Todo Mal repaso nefasto antes del examen

---
+++ 2DAW_JavaScript_DWEC/UD2/UD2_2_4_1 Extra_HojaEjercicios/01_Calculadora/OperacionBis.js
@@ -0,0 +1,49 @@
+/* class Operacion {
+
+    constructor(num1, num2) {
+        this.operando1 = num1;
+        this.operando2 = num2;
+        this._resultado = 0; // Inicializamos resultado
+    }
+
+    suma() {
+        this._resultado = this.operando1 + this.operando2;
+    }
+
+    resta() {
+        this._resultado = this.operando1 - this.operando2;
+    }
+
+    multiplicacion() {
+        this._resultado = this.operando1 * this.operando2;
+    }
+
+    division() {
+        this._resultado = this.operando1 / this.operando2;
+    }
+
+    eligeOperacion(signo) {
+        switch (signo) {
+            case '+':
+                this.suma();
+                break;
+            case '-':
+                this.resta();
+                break;
+            case '*':
+                this.multiplicacion();
+                break;
+            case '/':
+                this.division();
+                break;
+            default:
+                alert("Operación equivocada");
+                break;
+        }
+    }
+
+    get resultado() {
+        return this._resultado;
+    }
+}
+ */
---
+++ 2DAW_JavaScript_DWEC/UD2/UD2_2_4_1 Extra_HojaEjercicios/01_Calculadora/miCalculadora.js
@@ -0,0 +1,15 @@
+/* import { Operacion } from './Operacion.js';
+let op1 = parseFloat(document.getElementById("operando1").value);
+let op2 = parseFloat(document.getElementById("operando2").value);
+let signo = document.getElementById("signo").value;
+var operacion = new Operacion(op1, op2);
+operacion.eligeOperacion(signo);
+document.getElementById("resultado").innerText = operacion.resultado;
+
+function operar(operacion) {
+
+    operacion.eligeOperacion(signo);
+    document.getElementById("resultado").innerText = operacion.resultado;
+
+
+} */
